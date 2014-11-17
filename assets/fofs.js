ec5b27d5f71fe3a59988916165584fdacf193b49
Greg Kuwaye <greg.kuwaye@gmail.com>
2014-11-17 08:55:00 -0800

    Countdown, mobile fixes

--- assets/fofs.js
+++ assets/fofs.js
@@ -28,10 +28,9 @@ Fofs.prototype.draw = function() {
  * @returns {string}
  */
 Fofs.prototype.calculate = function() {
-  var utc    = Math.floor(new Date().getTime() / 1000),
-      sdiff  = utc - this.options.start,
-      ddiff  = Math.floor(sdiff / 86400),
-      binary = Number(ddiff).toString(2),
+  var dayDiff = this.getDayDiff(),
+      timeUntilNext = this.getTimeUntilNextDayString(),
+      binary = Number(dayDiff).toString(2),
       length = binary.length,
       pieces = [];
 
@@ -41,5 +40,56 @@ Fofs.prototype.calculate = function() {
     }
   }
 
-  return pieces.join(' + ') + ' <span class="total">(' + ddiff + ' days)</span>';
+  var str = '';
+  str += pieces.join(' + ');
+  str += ' <div class="total">' + dayDiff + ' days</div>';
+  str += ' <div class="next-in">Next fof time in ' + timeUntilNext + '</div>';
+  return str;
+};
+
+/**
+ * @returns {number}
+ */
+Fofs.prototype.getDayDiff = function() {
+  var secondsDiff = this.getSecondsDiff();
+  return Math.floor(secondsDiff / 86400);
+};
+
+/**
+ * @returns {number}
+ */
+Fofs.prototype.getSecondsDiff = function() {
+  var utc = Math.floor(new Date().getTime() / 1000);
+  return utc - this.options.start;
+};
+
+/**
+ * @returns {string}
+ */
+Fofs.prototype.getTimeUntilNextDayString = function() {
+  var totalSecUntilNext = this.getSecondsUntilNextDay(),
+      hoursUntilNext = Math.floor(totalSecUntilNext / 3600),
+      minUntilNext = Math.floor((totalSecUntilNext - (hoursUntilNext * 3600)) / 60),
+      secUntilNext = totalSecUntilNext - (hoursUntilNext * 3600) - (minUntilNext * 60);
+
+  return this.padWithZero(hoursUntilNext) + ':' +
+    this.padWithZero(minUntilNext) + ':' +
+    this.padWithZero(secUntilNext);
+};
+
+/**
+ * @returns {number}
+ */
+Fofs.prototype.getSecondsUntilNextDay = function() {
+  var secondsDiff = this.getSecondsDiff();
+  // Seconds in day minus number of seconds elapsed in this day.
+  return 86400 - (secondsDiff % 86400);
+};
+
+/**
+ * @param {number} val
+ * @return {string}
+ */
+Fofs.prototype.padWithZero = function(val) {
+  return val < 10 ? ('0' + val) : ('' + val);
 };
