bb82338d890311300a06061e111b9a2bc3566746
Greg Kuwaye <greg.kuwaye@gmail.com>
2016-08-27 21:05:45 -0700

    Simplify build process; exploit ES6

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,198 +1,110 @@
 module.exports = function(grunt) {
-  'use strict';
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-
-    babel: {
-      production: {
-        expand: true,
-        cwd: 'src/assets',
-        src: 'js/*.js',
-        dest: 'dist/assets'
-      }
-    },
-
-    clean: {
-      all: ['dist'],
-      css: ['dist/assets/css'],
-      html: ['dist/assets/*.html'],
-      img: ['dist/assets/img'],
-      js: ['dist/assets/js']
-    },
-
-    connect: {
-      server: {
-        options: {
-          port: 8000,
-          base: 'dist',
-          keepalive: true
-        }
-      }
-    },
-
-    cssmin: {
-      production: {
-        expand: true,
-        cwd: 'dist/assets/css',
-        src: '*.css',
-        dest: 'dist/assets/css'
-      }
-    },
-
-    copy: {
-      img: {
-        expand: true,
-        cwd: 'src/assets',
-        src: 'img/**',
-        dest: 'dist/assets'
-      },
-      js: {
-        expand: true,
-        cwd: 'src/assets',
-        src: 'js/*.js',
-        dest: 'dist/assets'
-      },
-      vendor: {
-        files: [
-          {
-             expand: true,
-             cwd: 'node_modules',
-             src: 'moment/moment.js',
-             dest: 'dist/assets/js'
-          },
-          {
-             expand: true,
-             cwd: 'node_modules/react/dist',
-             src: ['react.js', 'JSXTransformer.js'],
-             dest: 'dist/assets/js/react'
-          }
-        ]
-      }
-    },
-
-    eslint: {
-      all: ['Gruntfile.js', 'assets/**/*.js']
-    },
-
-    htmlmin: {
-      production: {
-        options: {
-          removeComments: true,
-          collapseWhitespace: true
+        babel: {
+            build: {
+                expand: true,
+                cwd: 'src/assets',
+                src: 'js/*.js',
+                dest: 'dist/assets'
+            }
         },
-        files: [
-          {
-            expand: true,
-            cwd: 'dist',
-            src: '*.html',
-            dest: 'dist'
-          }
-        ]
-      }
-    },
 
-    less: {
-      compile: {
-        options: {
-          paths: ['src/assets/less']
+        clean: {
+            all: ['dist'],
+            css: ['dist/assets/css'],
+            html: ['dist/assets/*.html'],
+            img: ['dist/assets/img'],
+            js: ['dist/assets/js']
         },
-        files: {
-          'dist/assets/css/style.css': 'src/assets/less/style.less'
+
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    base: 'dist',
+                    keepalive: true
+                }
+            }
+        },
+
+        copy: {
+            html: {
+                expand: true,
+                cwd: 'src',
+                src: '*.html',
+                dest: 'dist'
+            },
+            img: {
+                expand: true,
+                cwd: 'src/assets',
+                src: 'img/**',
+                dest: 'dist/assets'
+            }
+        },
+
+        eslint: {
+            all: ['Gruntfile.js', 'assets/**/*.js']
+        },
+
+        less: {
+            compile: {
+                options: {
+                    paths: ['src/assets/less']
+                },
+                files: {
+                    'dist/assets/css/style.css': 'src/assets/less/style.less'
+                }
+            }
+        },
+
+        watch: {
+            assets: {
+                files: 'src/assets/img/*',
+                tasks: [
+                    'clean:img',
+                    'copy:img'
+                ]
+            },
+            gruntfile: {
+                files: 'Gruntfile.js',
+                tasks: ['build']
+            },
+            js: {
+                files: ['src/assets/js/*.js'],
+                tasks: [
+                    'eslint',
+                    'clean:js',
+                    'babel:build'
+                ]
+            },
+            less: {
+                files: ['src/assets/less/*.less'],
+                tasks: ['less']
+            },
+            html: {
+                files: ['src/*.html'],
+                tasks: ['copy:html']
+            }
         }
-      }
-    },
+    });
 
-    preprocess: {
-      options: {
-        context: {
-          // This is the default, but be explicit for clarity
-          NODE_ENV: 'development'
-        }
-      },
-      development: {
-        src: 'src/index.html',
-        dest: 'dist/index.html'
-      },
-      production: {
-        src: 'src/index.html',
-        dest: 'dist/index.html',
-        options: {
-          context: {
-            NODE_ENV: 'production'
-          }
-        }
-      }
-    },
+    grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('gruntify-eslint');
 
-    watch: {
-      assets: {
-        files: 'src/assets/img/*',
-        tasks: [
-          'clean:img',
-          'copy:img'
-        ]
-      },
-      gruntfile: {
-        files: 'Gruntfile.js',
-        tasks: ['build:development']
-      },
-      js: {
-        files: ['src/assets/js/*.js'],
-        tasks: [
-          'eslint',
-          'clean:js',
-          'copy:js',
-          'copy:vendor',
-          'preprocess:development'
-        ]
-      },
-      less: {
-        files: ['src/assets/less/*.less'],
-        tasks: ['less']
-      },
-      html: {
-        files: ['src/*.html'],
-        tasks: ['less']
-      }
-    }
-  });
+    grunt.registerTask('build', [
+        'clean:all',
+        'eslint',
+        'less',
+        'copy',
+        'babel:build'
+    ]);
 
-  grunt.loadNpmTasks('grunt-babel');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-preprocess');
-  grunt.loadNpmTasks('gruntify-eslint');
-
-  // Development
-  grunt.registerTask('build:development', [
-    'clean:all',
-    'eslint',
-    'less',
-    'copy:img',
-    'copy:js',
-    'copy:vendor',
-    'preprocess:development'
-  ]);
-
-  // Production
-  grunt.registerTask('build:production', [
-    'clean:all',
-    'eslint',
-    'less',
-    'cssmin:production',
-    'copy:img',
-    'babel:production',
-    'preprocess:production',
-    'htmlmin:production'
-  ]);
-
-  // Default builds development and starts the server
-  grunt.registerTask('default', ['build:development', 'connect']);
+    grunt.registerTask('default', 'build');
 };
--- src/assets/js/fofs.js
+++ src/assets/js/fofs.js
@@ -1,136 +1,132 @@
-(function(moment) {
-  'use strict';
-
-  var Fofs = React.createClass({
-    getInitialState: function() {
-      return {
-        // Parse the static date once
-        start: moment(this.props.startDate)
-      };
+const Fofs = React.createClass({
+    getInitialState() {
+        return {
+            // Parse the static date once
+            start: moment(this.props.startDate),
+        };
     },
-    componentDidMount: function() {
-      this.tick();
-      this.interval = setInterval(this.tick, 500);
+    componentDidMount() {
+        this.tick();
+        this.interval = setInterval(this.tick, 500);
     },
-    componentWillUnmount: function() {
-      clearInterval(this.interval);
+    componentWillUnmount() {
+        clearInterval(this.interval);
     },
-    tick: function() {
-      var ms = moment() - this.state.start;
-      this.setState({
-        ms: ms,
-        days: Math.floor(moment.duration(ms).asDays())
-      });
+    tick() {
+        const ms = moment() - this.state.start;
+        this.setState({
+            ms,
+            days: Math.floor(moment.duration(ms).asDays())
+        });
     },
-    render: function() {
-      return (
-        <div>
-          <DaysAsPowersOfTwo days={this.state.days} />
-          <DaysDisplay days={this.state.days} />
-          <Countdown ms={this.state.ms} />
-        </div>
-      );
+    render() {
+        return (
+            <div>
+                <DaysAsPowersOfTwo days={this.state.days} />
+                <DaysDisplay days={this.state.days} />
+                <Countdown ms={this.state.ms} />
+            </div>
+        );
     }
-  });
+});
 
-  /**
-   * Renders days as a sum of powers of two.
-   */
-  var DaysAsPowersOfTwo = React.createClass({
+/**
+ * Renders days as a sum of powers of two.
+ */
+const DaysAsPowersOfTwo = React.createClass({
     getHtml: function() {
-      var binaryStr = Number(this.props.days).toString(2),
-          length = binaryStr.length;
+        const binaryStr = Number(this.props.days).toString(2);
+        const length = binaryStr.length;
 
-      return {
-        __html: binaryStr.split('').reduce(function(acc, bit, idx) {
-          if (+bit) {
-            var power = length - idx - 1;
-            acc.push('2<sup>' + power + '</sup>');
-          }
-          return acc;
-        }, []).join(' + ')
-      };
+        return {
+            __html: binaryStr.split('').reduce((acc, bit, idx) => {
+                if (+bit) {
+                    const power = length - idx - 1;
+                    acc.push('2<sup>' + power + '</sup>');
+                }
+                return acc;
+            }, []).join(' + ')
+        };
     },
-    render: function() {
-      return <div className="powers" dangerouslySetInnerHTML={this.getHtml()}></div>;
+    render() {
+        return <div className="powers" dangerouslySetInnerHTML={this.getHtml()}></div>;
     }
-  });
+});
 
-  /**
-   * Renders a simples "number of days" string.
-   */
-  var DaysDisplay = React.createClass({
-    dayOrDays: function() {
-      return this.props.days === 1 ? 'day' : 'days';
+/**
+* Renders a simples "number of days" string.
+*/
+const DaysDisplay = React.createClass({
+    dayOrDays() {
+        return this.props.days === 1 ? 'day' : 'days';
     },
-    render: function() {
-      return <div className="total">{this.props.days} {this.dayOrDays()}</div>;
+    render() {
+        return <div className="total">{this.props.days} {this.dayOrDays()}</div>;
     }
-  });
+});
 
-  /**
-   * Renders a countdown until the next day change.
-   */
-  var Countdown = React.createClass({
+/**
+* Renders a countdown until the next day change.
+*/
+const Countdown = React.createClass({
     getTime: function() {
-      var dayInMs = 86400000,
-          msUntilNext = dayInMs - (this.props.ms % dayInMs),
-          secUntilNext = Math.ceil(msUntilNext / 1000),
-          hours = Math.floor(secUntilNext / 3600),
-          minutes = Math.floor((secUntilNext - (hours * 3600)) / 60),
-          seconds = secUntilNext - (hours * 3600) - (minutes * 60);
+        const dayInMs = 86400000;
+        const msUntilNext = dayInMs - (this.props.ms % dayInMs);
+        const secUntilNext = Math.ceil(msUntilNext / 1000);
+        const hours = Math.floor(secUntilNext / 3600);
+        const minutes = Math.floor((secUntilNext - (hours * 3600)) / 60);
+        const seconds = secUntilNext - (hours * 3600) - (minutes * 60);
 
-      return [hours, minutes, seconds].map(function(value) {
-          return (value < 10 ? '0' : '') + value;
+        return [hours, minutes, seconds].map((value) => {
+            return (value < 10 ? '0' : '') + value;
         }).join(':');
     },
-    render: function() {
-      return (
-        <div className="next-in">
-          Next fof time in {this.getTime()}
-        </div>
-      );
+    render() {
+        return (
+            <div className="next-in">
+                Next fof time in {this.getTime()}
+            </div>
+        );
     }
-  });
+});
 
-  var DaysUntil = React.createClass({
-    getInitialState: function() {
-      return {
-        until: moment(this.props.untilDate, 'YYYY-MM-DD')
-      };
+const DaysUntil = React.createClass({
+    getInitialState() {
+        return {
+            until: moment(this.props.untilDate, 'YYYY-MM-DD')
+        };
     },
-    componentDidMount: function() {
-      this.tick();
-      this.interval = setInterval(this.tick, 500);
+    componentDidMount() {
+        this.tick();
+        this.interval = setInterval(this.tick, 500);
     },
-    componentWillUnmount: function() {
-      clearInterval(this.interval);
+    componentWillUnmount() {
+        clearInterval(this.interval);
     },
-    tick: function() {
-      var ms = this.state.until - moment();
-      var days = Math.max(0, Math.floor(moment.duration(ms).asDays()));
-      var word = days === 1 ? 'day' : 'days';
-      this.setState({
-        days: days,
-        word: word
-      });
+    tick() {
+        const ms = this.state.until - moment();
+        const days = Math.max(0, Math.floor(moment.duration(ms).asDays()));
+        const word = days === 1 ? 'day' : 'days';
+        this.setState({
+            days,
+            word,
+        });
     },
     render: function() {
-      return (
-        <span className="days-until">
-          {this.state.days} {this.state.word}
-        </span>
-      );
+        return (
+            <span className="days-until">
+                {this.state.days} {this.state.word}
+            </span>
+        );
     }
-  });
+});
 
-  React.render(
+ReactDOM.render(
     <Fofs startDate="2013-02-19 19:30:00-05:00" />,
     document.getElementById('fofs')
-  );
+);
 
-  React.render(
+ReactDOM.render(
     <DaysUntil untilDate="2016-09-24" />,
     document.getElementById('countdown-days')
-  );
-}(window.moment));
+);
